6c76b820b6610033a1388425022a4fcd68ba0242
Matheus Eduardo Loeblein <matheus.eai@gmail.com>
2022-09-30 17:09:46 -0300

    Add db

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from recipes import views
 
+# recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<id>/', views.recipe)
+    path('', views.home, name="home"),
+    path('recipes/<id>/', views.recipe, name="recipe")
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+from recipes.models import Recipe
+
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, "recipes\pages\home.html", context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
 
 
 def recipe(request, id):
     return render(request, "recipes\pages\pecipe-view.html", context={
         'recipe': make_recipe(),
+        'is_detail_page': True,
     })
