436df7444b78f2ab140fd1e9f36d2c69c9bf9401
Matheus Eduardo Loeblein <matheus.eai@gmail.com>
2022-10-03 17:15:08 -0300

    category url update

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 from email.policy import default
 from pickle import TRUE
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -28,7 +29,7 @@ class Recipe(models.Model):
     cover = models.ImageField(
         upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
